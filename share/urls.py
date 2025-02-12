57ad25ccc631ff9be275a2d0b82572d932b8d9db
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-02-12 16:36:22 +0530

    added links

--- share/urls.py
+++ share/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import share_song, share_playlist
+from .views import share_song, share_playlist, share_album, share_artist
 
 urlpatterns = [
     path("content/songs/<int:id>", share_song),
     path("content/playlists/<int:id>", share_playlist),
+    path("content/albums/<int:id>", share_album),
+    path("content/artists/<int:id>", share_artist),
 ]
