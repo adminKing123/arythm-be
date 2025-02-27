0863ab35118120aa2627c1902548d6fe6ab448d3
adminKing123 <116816518+adminKing123@users.noreply.github.com>
2025-02-27 19:06:57 +0530

    Update functions.py

--- songs/functions.py
+++ songs/functions.py
@@ -1,5 +1,24 @@
+# from config import CONFIG
+# import random
+
+# def get_slides(request):
+#     return random.sample(CONFIG["slides"], 3)
+
 from config import CONFIG
 import random
 
 def get_slides(request):
-    return random.sample(CONFIG["slides"], 3)
+    interested_ids = [2, 3]
+    slides = CONFIG["slides"]
+
+    # Get slides matching interested_ids
+    interested_slides = [slide for slide in slides if slide["id"] in interested_ids]
+
+    # Get remaining slides excluding interested ones
+    remaining_slides = [slide for slide in slides if slide["id"] not in interested_ids]
+
+    # Fill remaining slots randomly
+    num_to_pick = 3 - len(interested_slides)
+    selected_slides = interested_slides + random.sample(remaining_slides, num_to_pick)
+
+    return selected_slides
